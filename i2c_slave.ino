539b5bc4dd1e34c00e526d49dc332d4c05a7df59
2cool <2coolcpp@gmail.com>
2017-05-17 23:29:14 +0300

    17082017

--- i2c_slave.ino
+++ i2c_slave.ino
@@ -2,12 +2,13 @@
 
 
 
-
-
+#define BEEP 13
+uint8_t beeps_coder[] = { B00001000,B00001100,B00001110,0,0,0,0,0,0,0,0,0,0,0,0};//4 beeps if 0 short 1 long beep
 int16_t readBuffer[6];
 float fb[3];
 void setup() {
 //	Serial.begin(115200);
+	pinMode(BEEP, OUTPUT); //BEEP
 	pinMode(5, OUTPUT);//t0
 	pinMode(6, OUTPUT);
 
@@ -18,7 +19,7 @@ void setup() {
 	pinMode(10, OUTPUT);
 
 
-	int val = 254;
+	int val = 127;
 	//all 2000hz in 16mhz
 	TCCR0A = 163;
 	TCCR0B = 3;
@@ -60,16 +61,21 @@ int temp;
 	if (Wire.available()) {				\
 		temp = Wire.read();				\
 		n=temp;							\
-		}								
-	
+		}	
 
+enum COMMANDS_BIT { SOUND_ON = 1,BEEP_CODE=30 };
+
+
+volatile uint8_t beep_code;
+volatile uint8_t cnt = 0;
+volatile bool beep_on=true;
+uint8_t old_cnt = 0;
+uint16_t err = 0;
 
 void receiveEvent(int countToRead) {
 
 	int av = Wire.available();
 	if (av == 4) {
-		//Serial.println(8);
-
 		writePWM(OCR0A)
 		writePWM(OCR0B)
 		writePWM(OCR2A)
@@ -79,11 +85,18 @@ void receiveEvent(int countToRead) {
 		writePWMg(OCR1A)
 		writePWMg(OCR1B)
 	}
-	else {
+	else  if (av == 1) {
+		uint8_t command = Wire.read();
+		beep_on		= command&SOUND_ON;
+		beep_code	= (command&BEEP_CODE) << 1;
+		
+	}
+	else
+	{
 		while (Wire.available())  
 			Wire.read();
 	}
-
+	cnt++;
 }
 
 //	A0	-	3v from balance
@@ -102,16 +115,94 @@ void requestEvent() {
 }
 #define DELAY 1
 
+/*/if no commands from orange pi we will shutdown motors or we can read data from mpu and stabilize copter for
+	it down out*/
+void stop_motors() {
+	OCR0A = 127;
+	OCR0B = 127;
+	OCR2A = 127;
+	OCR2B = 127;
+
+}
+bool beep_bit = false;
+uint8_t beep_cnt = 0;
+uint16_t beep_code_cnt;
+uint8_t  beep_code_n;
+bool beep_code_on = false;
 
 
-void loop() {
 
+void beeps_code() {
+	beep_code_cnt++;
+	if (beep_code_on == false) {
+		if (beep_code_cnt > 150) {
+			beep_code_cnt = 0;
+			beep_code_on = true;
+		}
+	}
+	else {
+		bool long_beep = (beeps_coder[beep_code] << beep_code_n) & 1;
+		if (beep_code_cnt > (long_beep) ? 600 : 300) {
+			beep_code_cnt = 0;
+			beep_code_n++;
+			beep_code_on = false;
+			if (beep_code_n == 4) {
+				beep_code_cnt = 0;
+				beep_code_n = 0;
+				beep_code = 0;
+				beep_code_on = false;
+			}
+
+		}
+		else
+			digitalWrite(BEEP, beep_bit ^= true);
+	}
+}
+
+void beep() {
+	if (beep_code) {
+		beeps_code();
+	}else
+		if (beep_on) {
+			uint8_t beep_cnd = 255;
+			if (fb[2] > 300)
+				beep_cnd = (fb[2] < 900) ? 2 : 1;
+			else
+				if (fb[0] > 300 || fb[1] > 300)
+					beep_cnd = 0;
+
+			if (beep_cnd == beep_cnt) {
+				beep_cnt ^= beep_cnt;//=0
+				digitalWrite(BEEP, beep_bit ^= true);
+			}
+			else
+				beep_cnt++;
+		}
+}
+void update_voltage() {
 	fb[0] += ((float)analogRead(0)- fb[0])*0.03;
+	beep();
 	delay(DELAY);
 	fb[1] += ((float)analogRead(1) - fb[1])*0.03;
+	beep();
 	delay(DELAY);
 	fb[2] += ((float)analogRead(2) - fb[2])*0.03;
-	delay(DELAY);	
+	beep();
+	delay(DELAY);
+}
+
+void loop() {
+	if (cnt != old_cnt) {
+		old_cnt = cnt;
+		err = 0;
+		
+	}
+	else {
+		err++;
+		if (err > 300)
+			stop_motors();
+	}
+	update_voltage();
 //	Serial.println(fb[2]);
 //	Serial.println(analogRead(2));
 }
